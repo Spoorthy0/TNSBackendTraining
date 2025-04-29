d99f4bd6f7a10749d06d516f618d6e7ecad3411c
Spoorthy0 <spoorthyyedla@gmail.com>
2025-04-29 22:39:12 +0530

    VariableTypes(local,instance,static) example Program

---
+++ TNSBackendTraining/src/com/tnsif/sriindu/variablesdemo/VariableMain.java
@@ -0,0 +1,13 @@
+package com.tnsif.sriindu.variablesdemo;
+
+public class VariableMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VariableTypes obj=new VariableTypes();
+		obj.number=101;
+		obj.display();
+
+	}
+
+} 
---
+++ TNSBackendTraining/src/com/tnsif/sriindu/variablesdemo/VariableTypes.java
@@ -0,0 +1,10 @@
+package com.tnsif.sriindu.variablesdemo;
+public class VariableTypes {
+		//instance variable
+		int number;
+		static String name="Spoorthy";
+		public void display() {
+			System.out.println(number+" "+name);
+		}
+
+}
